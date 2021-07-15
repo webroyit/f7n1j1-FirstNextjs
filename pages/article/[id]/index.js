59743d4c8f085e4bf717a43d02909bdaf958aab6
royweb <xytank@gmail.com>
2021-07-15 12:32:47 -0400

    Use useRouter from next/router to get parameters from route

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,13 @@
+import {useRouter} from 'next/router'
+
 const article = () => {
+    // Get the parameters from URL or route
+    const router = useRouter()
+    const {id} = router.query
+
     return (
         <div>
-            An article
+            An article {id}
         </div>
     )
 }
