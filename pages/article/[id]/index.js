2d1d7c33c499732b2cff981d9de471a14b663ef4
royweb <xytank@gmail.com>
2021-07-17 12:33:30 -0400

    Refactor to use API Data

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import {server} from '../../../config'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 
@@ -16,11 +17,9 @@ const article = ({ article }) => {
     )
 }
 
-// 'context' contain parameters from route
-// When using getStaticProps that uses parameters from route, you need to call getStaticPaths to generate paths
 export const getStaticProps = async context => {
     const res = await fetch(`
-        https://jsonplaceholder.typicode.com/posts/${context.params.id}
+        ${server}/api/articles/${context.params.id}
     `)
 
     const article = await res.json()
@@ -34,7 +33,7 @@ export const getStaticProps = async context => {
 
 export const getStaticPaths = async () => {
     // Get all articles
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles`)
     const articles = await res.json()
 
     // Get all article ID
@@ -48,5 +47,38 @@ export const getStaticPaths = async () => {
         fallback: false     // return 404 page if the data does not exist
     }
 }
+
+// 'context' contain parameters from route
+// When using getStaticProps that uses parameters from route, you need to call getStaticPaths to generate paths
+// export const getStaticProps = async context => {
+//     const res = await fetch(`
+//         https://jsonplaceholder.typicode.com/posts/${context.params.id}
+//     `)
+
+//     const article = await res.json()
+
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+// export const getStaticPaths = async () => {
+//     // Get all articles
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+//     const articles = await res.json()
+
+//     // Get all article ID
+//     const ids = articles.map(article => article.id)
+
+//     // Format the article ids as array of objects
+//     const paths = ids.map(id => ({params: {id: id.toString()}}))
+
+//     return {
+//         paths,      // ex => {params: {id: '1', id: '2'}}
+//         fallback: false     // return 404 page if the data does not exist
+//     }
+// }
 
 export default article
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import {server} from '../config'
+
 // For custom titles, meta tags, keywords, descriptions, ...
 import Head from 'next/head'
 
@@ -18,11 +20,8 @@ export default function Home({ articles }) {
   )
 }
 
-// getStaticProps fetch data at build time
-// server side props fetch data on every request
-// get static path to dynamically generate paths based on the data that is fetched
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return {
@@ -31,3 +30,17 @@ export const getStaticProps = async () => {
     }
   }
 }
+
+// getStaticProps fetch data at build time
+// server side props fetch data on every request
+// get static path to dynamically generate paths based on the data that is fetched
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
